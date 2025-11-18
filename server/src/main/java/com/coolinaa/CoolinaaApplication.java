afc880d953db5a6922abb0506596cf3330c4cc4c
sayranovv <sayranoveldar@gmail.com>
2025-11-18 13:26:31 +0500

    create spring backend

---
+++ server/src/main/java/com/coolinaa/CoolinaaApplication.java
@@ -0,0 +1,13 @@
+package com.coolinaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolinaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolinaaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/coolinaa/CoolinaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolinaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolinaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
