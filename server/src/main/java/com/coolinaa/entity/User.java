b240be39ccf0e16071b82c8d59ec79fc0e81a80a
sayranovv <sayranoveldar@gmail.com>
2025-11-19 18:18:07 +0500

    update user entity

--- server/src/main/java/com/coolinaa/entity/User.java
+++ server/src/main/java/com/coolinaa/entity/User.java
@@ -1,24 +1,57 @@
 package com.coolinaa.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Setter
+    @Column(nullable = false, length = 50)
     private String username;
 
-    public User() {}
+    @Column(nullable = false, length = 100)
+    private String email;
 
-    public User(String username) {
-        this.username = username;
+    @Column(name = "password_hash", nullable = false, length = 255)
+    private String passwordHash;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Recipe> recipes = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Review> reviews = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserIngredient> userIngredients = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate() {
+        if  (createdAt == null) {
+            createdAt = OffsetDateTime.now();
+        }
     }
 
 }
