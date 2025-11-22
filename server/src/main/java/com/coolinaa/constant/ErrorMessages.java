8015c7021906e2723a911f536c3c3ef5e981bd67
sayranovv <sayranoveldar@gmail.com>
2025-11-22 16:01:36 +0500

    create error messages constants

---
+++ server/src/main/java/com/coolinaa/constant/ErrorMessages.java
@@ -0,0 +1,16 @@
+package com.coolinaa.constant;
+
+public class ErrorMessages {
+
+    public static final String INVALID_JWT = "invalid jwt token";
+    public static final String EXPIRED_JWT = "expired jwt token";
+    public static final String UNSUPPORTED_JWT = "unsupported jwt token";
+    public static final String JWT_CLAIMS_EMPTY = "jwt claims r empty";
+    public static final String USER_NOT_FOUND = "user not found";
+    public static final String UNAUTHORIZED = "auth is required";
+    public static final String ACCESS_DENIED = "access denied";
+    public static final String INVALID_CREDENTIALS = "invalid credentials";
+    public static final String USERNAME_ALREADY_EXISTS = "username already exists";
+    public static final String EMAIL_ALREADY_EXISTS = "email already exists";
+
+}
