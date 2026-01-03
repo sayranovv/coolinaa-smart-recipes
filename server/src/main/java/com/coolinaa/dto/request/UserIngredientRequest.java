bff44b2c73bc2981ed37e32a2e9dbe0ac40b6130
sayranovv <sayranoveldar@gmail.com>
2026-01-03 05:09:31 +0500

    fix dto

--- server/src/main/java/com/coolinaa/dto/request/UserIngredientRequest.java
+++ server/src/main/java/com/coolinaa/dto/request/UserIngredientRequest.java
@@ -2,10 +2,18 @@ package com.coolinaa.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserIngredientRequest {
 
     @NotNull(message = "ingredient id is required")
