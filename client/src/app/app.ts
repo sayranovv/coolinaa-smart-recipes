554020d09054533713c78950c056cc7f85966293
sayranovv <sayranoveldar@gmail.com>
2026-01-04 16:31:26 +0500

    [frontend] update app entry point

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private readonly auth = inject(AuthService);
   protected readonly title = signal('client');
+
+  ngOnInit() {
+    this.auth.initialize().subscribe();
+  }
 }
